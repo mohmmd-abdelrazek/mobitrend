672add05f17ca73f4fa780a1ed54f8cc3f9bc28b
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-04-20 23:49:52 +0200

    remove unused functions

--- frontend/src/services/queries.ts
+++ frontend/src/services/queries.ts
@@ -1,39 +1,5 @@
 import useSWR from "swr";
-import { League } from "../types/league";
-import { Match } from "../types/match";
-import { useParams } from "next/navigation";
-
-const extractLeagueId = (leagueSlug: string | string[] | undefined): number | null => {
-  if (typeof leagueSlug !== 'string') return null;
-  
-  const possibleId = leagueSlug.split('-').pop();
-  const leagueId = Number(possibleId);
-  
-  return !isNaN(leagueId) && leagueId > 0 ? leagueId : null;
-};
 
 export function useAuth() {
   return useSWR("/auth/status");
-}
-
-export function useLeagues() {
-  return useSWR<League[]>("/leagues");
-}
-
-export function useLeague() {
-  const { leagueSlug } = useParams();
-  const leagueId = extractLeagueId(leagueSlug);
-  return useSWR<League>(leagueId ? `/league/${leagueId}` : null);
-}
-
-export function useTeams() {
-  const { leagueSlug } = useParams();
-  const leagueId = extractLeagueId(leagueSlug);  
-  return useSWR(leagueId ? `/teams/${leagueId}` : null);
-}
-
-export function useSchedule() {
-  const { leagueSlug } = useParams();
-  const leagueId = extractLeagueId(leagueSlug); 
-  return useSWR(leagueId ? `/league/${leagueId}/schedule` : null);
 }
