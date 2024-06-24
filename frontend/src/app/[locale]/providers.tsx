5c49d2b686ed47f35b37bd1945a4018013287daf
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-06-24 21:32:41 +0300

    fix env in next config

--- frontend/src/app/[locale]/providers.tsx
+++ frontend/src/app/[locale]/providers.tsx
@@ -1,20 +1,20 @@
-"use client"
-import { SWRConfig } from "swr"
-import fetcher from "@/src/services/fetcher"
+"use client";
+import { SWRConfig } from "swr";
+import fetcher from "@/src/services/fetcher";
 
-export function SWRProvider({children} : {children: React.ReactNode}) {
-    return <SWRConfig value={{fetcher}}>{children}</SWRConfig>
+export function SWRProvider({ children }: { children: React.ReactNode }) {
+  return <SWRConfig value={{ fetcher }}>{children}</SWRConfig>;
 }
 
-import React, { useState, useContext, useEffect } from 'react';
-import { loadStripe, Stripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
+import React, { useState, useContext, useEffect } from "react";
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
 const StripeContext = React.createContext<Stripe | null>(null);
 
 export const useStripe = () => useContext(StripeContext);
 
-export const StripeProvider = ({children} : {children: React.ReactNode}) => {
+export const StripeProvider = ({ children }: { children: React.ReactNode }) => {
   const [stripe, setStripe] = useState<Stripe | null>(null);
 
   useEffect(() => {
